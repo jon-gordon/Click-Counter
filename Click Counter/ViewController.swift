b6318f03ebb8e0030c528baa36381e3326c47b48
Jon Gordon <jon@jongor-software.com>
2015-05-10 22:41:31 +0100

    Extended work - second mirror label, decrement button and toggle colour

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,34 +11,89 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 0
-    var label:UILabel!
+    var label1:UILabel!
+    var label2:UILabel!
+    var toggle:Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Label
-        var label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "0"
+        // Label 1
+        var label1 = UILabel()
+        label1.frame = CGRectMake(150, 150, 60, 60)
+        label1.text = "0"
 
-        self.view.addSubview(label)
+        self.view.addSubview(label1)
 
-        self.label = label
+        self.label1 = label1
 
-        // Button
-        var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
 
-        self.view.addSubview(button)
+        // Label 2
+        var label2 = UILabel()
+        label2.frame = CGRectMake(220, 150, 60, 60)
+        label2.text = "0"
 
-        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(label2)
+
+        self.label2 = label2
+
+
+        // Button Increment
+        var button1 = UIButton()
+        button1.frame = CGRectMake(150, 250, 100, 60)
+        button1.setTitle("Increment", forState: .Normal)
+        button1.setTitleColor(UIColor.blueColor(), forState: .Normal)
+
+        self.view.addSubview(button1)
+
+        button1.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+
+
+        // Button Decrement
+        var button2 = UIButton()
+        button2.frame = CGRectMake(250, 250, 100, 60)
+        button2.setTitle("Decrement", forState: .Normal)
+        button2.setTitleColor(UIColor.blueColor(), forState: .Normal)
+
+        self.view.addSubview(button2)
+
+        button2.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+
+
+        // Button Toggle
+        var button3 = UIButton()
+        button3.frame = CGRectMake(150, 350, 120, 60)
+        button3.setTitle("Toggle Colour", forState: .Normal)
+        button3.setTitleColor(UIColor.blueColor(), forState: .Normal)
+
+        self.view.addSubview(button3)
+
+        button3.addTarget(self, action: "toggleColour", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     func incrementCount() {
         self.count++
-        self.label.text = "\(self.count)"
+        updateLabels()
+    }
+
+    func decrementCount() {
+        self.count--
+        updateLabels()
+    }
+
+    func updateLabels() {
+        self.label1.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+    }
+
+    func toggleColour() {
+        if toggle {
+            self.view.backgroundColor = UIColor.whiteColor()
+            toggle = false
+        } else {
+            self.view.backgroundColor = UIColor.grayColor()
+            toggle = true
+        }
     }
 }
 
